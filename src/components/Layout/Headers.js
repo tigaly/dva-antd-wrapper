2350c54c9c6ea15cf5dc2e1a809f0d82457ee39a
tigaly <tigaly@qq.com>
2017-04-14 00:11:38 +0800

    update header less

--- src/components/Layout/Headers.js
+++ src/components/Layout/Headers.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import styles from './Headers.less';
 
 const { Header, Content, Footer } = Layout;
 
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
+
 const Headers = (props) => {
+  const handleClick = (e) => {
+    this.setState({
+      current: e.key,
+    });
+  };
   return (
-    <Layout>
-      <Header style={{ position: 'fixed', width: '100%' }}>
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={['2']}
-          style={{ lineHeight: '64px' }}
-        >
-          <Menu.Item key="1">Home</Menu.Item>
-          <Menu.Item key="2">List</Menu.Item>
-          <Menu.Item key="3">App</Menu.Item>
-        </Menu>
+    <Layout >
+      <Header className={styles.header}>
+        <div className={styles.inner}>
+          <Menu
+            onClick={handleClick}
+            selectedKeys="mail"
+            mode="horizontal"
+            style={{ lineHeight: '64px' }}
+          >
+            <Menu.Item key="mail">
+              <Icon type="mail" />Navigation One
+            </Menu.Item>
+            <Menu.Item key="app" disabled>
+              <Icon type="appstore" />Navigation Two
+            </Menu.Item>
+            <SubMenu title={<span><Icon type="setting" />Navigation Three - Submenu</span>}>
+              <MenuItemGroup title="Item 1">
+                <Menu.Item key="setting:1">Option 1</Menu.Item>
+                <Menu.Item key="setting:2">Option 2</Menu.Item>
+              </MenuItemGroup>
+              <Menu.Item key="setting:3">Option 3</Menu.Item>
+              <Menu.Item key="setting:4">Option 4</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="alipay">
+              <a href="https://ant.design" target="_blank" rel="noopener noreferrer">Navigation Four - Link</a>
+            </Menu.Item>
+          </Menu>
+        </div>
       </Header>
-      <Content style={{ padding: '0 50px', marginTop: 64 }}>
+      <Content style={{ padding: '0 25px', marginTop: 16 }}>
         <Breadcrumb style={{ margin: '12px 0' }}>
           <Breadcrumb.Item>Home</Breadcrumb.Item>
           <Breadcrumb.Item>List</Breadcrumb.Item>
